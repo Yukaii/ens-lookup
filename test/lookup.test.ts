916d3ce39e6091517b806ca601f37c4ff579e411
Yukai Huang <yukaihuangtw@gmail.com>
2022-08-24 16:46:18 +0800

    style: lint

--- test/lookup.test.ts
+++ test/lookup.test.ts
@@ -6,8 +6,6 @@ test("resolve ens domain", async (t) => {
 
   t.true(result.found);
   t.is(result.address, "0xd8da6bf26964af9d7eed9e03e53415d37aa96045");
-
-  console.log(result)
 });
 
 test("resolve address", async (t) => {
